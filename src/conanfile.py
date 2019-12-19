ce37a42e47b28d2003a2bcb788597aee1de7f60e
Lance Wallis <bleudeveloper@gmail.com>
2019-12-19 16:47:30 -0600

    Package 'cpprestsdk' breaking bug fixed upstream.

--- src/conanfile.py
+++ src/conanfile.py
@@ -7,10 +7,7 @@ class FactorioFrameHost(ConanFile):
     default_options = {"cpprestsdk:shared": True, "OpenSSL:shared": True}
 
     def requirements(self):
-        # OpenSSL currently has a case sensitivy bug with windows.
-        # Will reactivate upon merge.
-        ### self.requires.add("cpprestsdk/2.10.14@bincrafters/stable")
-        self.requires.add("boost/1.71.0")
+        self.requires.add("cpprestsdk/2.10.14@bincrafters/stable")
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
