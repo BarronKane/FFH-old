a239fa3d06bd80ea41f2baaeecc44763df8ad727
Lance Wallis <bleudeveloper@gmail.com>
2019-12-18 19:27:27 -0600

    Add wait to exe.

--- src/factorio-frame-host/test/main.cpp
+++ src/factorio-frame-host/test/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 #include "factorio-frame-host.h"
 
 int main()
@@ -5,4 +8,9 @@ int main()
     factorio ffh;
 
     ffh.test();
+
+    // Hack to fake a pause.
+    std::string wait;
+    std::cout << "Press enter to continue...";
+    std::getline(std::cin, wait);
 }
