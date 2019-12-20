c45cb58f24b56ea73d260c3066f31f75dabbac11
Lance Wallis <bleudeveloper@gmail.com>
2019-12-20 02:06:54 -0600

    C++17 compiling & test.

--- src/ffh-server/private/main.cpp
+++ src/ffh-server/private/main.cpp
@@ -2,7 +2,12 @@
 
 #include "factorio-frame-host.h"
 
+// C++17 quick constexpr lambda test.
+constexpr int addOne(int n) {
+    return [n] {return n + 1; }();
+}
+
 int main(int argc, char *argv[])
 {
-    
+    static_assert(addOne(1) == 2);
 }
