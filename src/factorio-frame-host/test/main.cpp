4764f0c32dc364eea378dbc48a4dc9d884b1aaa5
Lance Wallis <bleudeveloper@gmail.com>
2019-12-20 20:03:57 -0600

    Stub testing until implimentations mature.

--- src/factorio-frame-host/test/main.cpp
+++ src/factorio-frame-host/test/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <string>
 
-#include "factorio-frame-host.h"
-
 int main()
 {
-    factorio ffh;
-
-    ffh.test();
+    // TODO: Proper tests.
+    // This executable will remain stubbed until implimentations mature.
 
     // Hack to fake a pause.
     std::string wait;
