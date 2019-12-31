4d04dccdaa7cb5cf670dfd533a8a449d7f6910df
Lance Wallis <bleudeveloper@gmail.com>
2019-12-30 21:39:01 -0600

    Comprehensive build script improvements.

--- build.bat
+++ build.bat
@@ -2,6 +2,17 @@
 setlocal enableextensions
 pushd "%~dp0"
 
+CALL :GETPARENT PARENT
+IF /I "%PARENT%" == "powershell" goto :ISPOWERSHELL
+IF /I "%PARENT%" == "pwsh" goto :ISPOWERSHELL
+
+call env\Scripts\activate.bat
+if ERRORLEVEL 1 goto error 
+
+goto :build 
+
+:build 
+
 md build
 pushd build
 conan install ..\src\ --build missing -s build_type=Debug
@@ -9,9 +20,20 @@ cmake ..\src\ -G "Visual Studio 16 2019"
 cmake --build .
 popd
 
-if ERRORLEVEL 1 goto error
+goto :EOF 
+
+:ISPOWERSHELL 
+
+echo POWERSHELL DETECTED,
+echo you may have to run 'env/Scripts/activate.ps1 manually.
+
+goto :build 
+
+:error 
+echo Python environment not setup, see README.md
 goto :EOF
 
-:error
-rem Error occured, waiting for keypress.
-pause
+:GETPARENT
+SET "PSCMD=$ppid=$pid;while($i++ -lt 3 -and ($ppid=(Get-CimInstance Win32_Process -Filter ('ProcessID='+$ppid)).ParentProcessID)) {}; (Get-Process -EA Ignore -ID $ppid).Name"
+for /f "tokens=*" %%i in ('powershell -noprofile -command "%PSCMD%"') do SET %1=%%i
+goto :EOF
--- build.sh
+++ build.sh
@@ -6,8 +6,12 @@ cd "`dirname "$0"`"
 
 mkdir -p build/
 
+source $(pwd)/env/Scripts/activate
+
 pushd build/
 conan install ..\src\ --build missing -s build_type=Debug
 cmake ../src/
 cmake --build .
 popd > /dev/null
+
+deactivate
