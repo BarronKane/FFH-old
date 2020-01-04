a162c8375417f352eda40cff788f349b388dbbcc
Lance Wallis <bleudeveloper@gmail.com>
2020-01-03 21:15:33 -0600

    Flesh out preliminary factorio subprocess handling.

--- src/frame-host/private/subprocess/factorio_process.cpp
+++ src/frame-host/private/subprocess/factorio_process.cpp
@@ -8,16 +8,34 @@ factorio_process::factorio_process()
 
 }
 
-int factorio_process::spawn(boost::filesystem::path p, std::string args)
+void factorio_process::launch(boost::filesystem::path p, std::string args)
 {
 
-	boost::process::child c(p, args);
+	factorio_subprocess = bp::child(p, args, bp::std_out > pipe_stream);
 
-	// Temporary.
-	c.wait();
-	int result = c.exit_code();
-	return result;
+	pipe_stream_reader = std::thread([&]()
+		{
+			std::string line;
+			while (getline(pipe_stream, line))
+			{
+				// TODO: This should be piped to log/cout.
+				// TODO: Possible deadlock if read is attempted after process exit.
+			}
+		});
 
+}
+
+factorio_process::~factorio_process()
+{
+	if (pipe_stream_reader.joinable())
+	{
+		pipe_stream_reader.join();
+	}
+	if (factorio_subprocess.running())
+	{
+		// TODO: Force shutdown/safety.
+		factorio_subprocess.wait();
+	}
 }
 
 } // namespace factorio
--- src/frame-host/public/subprocess/factorio_process.h
+++ src/frame-host/public/subprocess/factorio_process.h
@@ -2,32 +2,40 @@
 
 #include <string>
 #include <vector>
+#include <thread>
 
+#include <boost/noncopyable.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/process.hpp>
+
+namespace bp = boost::process;
 
 namespace factorio
 {
 
 // TODO: This may end up a singleton or otherwise noncopyable.
-class factorio_process
+class factorio_process : private boost::noncopyable
 {
 
 public:
 
 	factorio_process();
+	~factorio_process();
 
 	
 
-	int spawn(boost::filesystem::path p, std::string args);
+	void launch(boost::filesystem::path p, std::string args);
 
 private: 
 
 	// TODO: Error check for *nix/win paths.
-	boost::filesystem::path path;
+	boost::filesystem::path path_;
+	std::vector<std::string> args_;
+	
+	bp::child factorio_subprocess;
+	bp::ipstream pipe_stream;
 
-	std::vector<std::string> args;
-
+	std::thread pipe_stream_reader;
 };
 
 } // namespace factorio
